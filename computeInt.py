cde828f860568df92679fec3cd6ed8c61a99c438
YUNHANKYU <bhgt4563@gmail.com>
2019-04-09 04:24:24 +0900

    make helloworld.py

---
+++ computeInt.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+print(int(3.3))
+print(int('10'))
+print(int(5/2))
+
+print(type(10))
+
+print(divmod(5, 2))
+
+quotient, remainder = divmod(5, 2)
+print("quotient is " ,quotient, remainder)
+
+ 
