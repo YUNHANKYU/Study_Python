f060950409ba8fdc2f76e503ca2e5c9649a51ff3
YUNHANKYU <bhgt4563@gmail.com>
2019-04-13 03:45:11 +0900

    6.1,2 변수 사용하기

---
+++ basics/variable.py
@@ -0,0 +1,47 @@
+x,y,z = 10,20,30
+
+print(x)
+print(y)
+print(z)
+
+a=b=c=5
+
+print(a)
+print(b)
+print(c)
+
+s, t = 100, 200
+
+s , t = t, s
+
+print(s)
+print(t)
+
+
+# 변수 삭제하기
+
+f = 10
+del f
+#print(f)
+
+
+# 빈 변수 만들기
+
+n = None
+print(n)
+
+# 산술 연산 후 할당 연산자 사용
+
+w = 10
+
+w = w + 10
+
+print(w)
+
+i = 10
+i += 10 
+print(i)
+
+# 부호 붙이기
+print(-w)
+print(-i)
